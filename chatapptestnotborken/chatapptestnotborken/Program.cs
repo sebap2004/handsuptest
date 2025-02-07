870adc8c4b5c35f3baaf1c1d163ff9a92f4e6bf2
prej2_22 <prej2_22@uni.worc.ac.uk>
2025-02-07 16:20:27 +0000

    first

---
+++ chatapptestnotborken/chatapptestnotborken/Program.cs
@@ -0,0 +1,39 @@
+using chatapptestnotborken.Client.Pages;
+using chatapptestnotborken.Components;
+using chatapptestnotborken.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapHub<ChatHub>("/chatHub");
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(chatapptestnotborken.Client._Imports).Assembly);
+
+app.Run();
